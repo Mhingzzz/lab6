f6dcb4039752393421d4e5c671ad0cd6df474e0b
mm <nathaphong_p@cmu.ac.th>
2021-11-26 16:09:41 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -6,16 +6,16 @@ int main(){
 
     cout << "Enter an integer: ";
     cin >> i;
-    while (i > 0)
+    while (i != 0)
     {
-        cout << "Enter an integer: ";
-        cin >> i;
         if (i % 2 == 0){
             even += 1;
             }
         else {
             odd += 1;
             }
+        cout << "Enter an integer: ";
+        cin >> i;
     }
     cout << "#Even numbers = " <<even <<endl;
     cout << "#Odd numbers = " <<odd; 
